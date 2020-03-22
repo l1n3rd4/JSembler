6207c81d441214aff5b2027bf0f3067a6e7e792b
Leonardo Henrique Guimarães <45611814+l1n3rd4@users.noreply.github.com>
2020-03-22 14:14:12 -0300

    Add files via upload

---
+++ src/arquivo/Aplicacao.java
@@ -0,0 +1,9 @@
+package arquivo;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/arquivo/Arquivo.java
@@ -0,0 +1,40 @@
+package arquivo;
+
+public class Arquivo {
+
+	public void lerComando(String opcao) {
+		
+		switch(opcao) {
+			case "add":
+				break;
+			case "addi":
+				break;
+			case "sub":
+				break;
+			case "mul":
+				break;
+			case "div":
+				break;
+			case "neg":
+				break;
+			case "lw":
+				break;
+			case "sw":
+				break;
+			case "j":
+				break;
+			case "jr":
+				break;
+			case "beq":
+				break;
+			case "bnq":
+				break;
+			case "slt":
+				break;
+			case "slti":
+				break;
+				
+				
+		}
+	}
+}
