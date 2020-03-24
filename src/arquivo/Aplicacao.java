2aa901ef01ae7dcf7e17869d7659faf326d4db02
Leonardo Henrique <leonardo.guimaraes20@icloud.com>
2020-03-24 00:04:00 -0300

    Alterações

--- src/arquivo/Aplicacao.java
+++ src/arquivo/Aplicacao.java
@@ -5,6 +5,8 @@ import Assembly_Binario.Binario;
 public class Aplicacao {
 
 	public static void main(String[] args) {
+		LeituraArquivo leitura = new LeituraArquivo();
+		GravarArquivo escrita = new GravarArquivo();
 		Binario binario = new Binario();
 		
 		System.out.println(binario.Assembly_Para_Binario("add"));
---
+++ src/arquivo/LeituraArquivo.java
@@ -0,0 +1,40 @@
+package arquivo;
+
+public class LeituraArquivo {
+
+	public void abrirArquivo(String nomeArquivo){	
+		try {
+			entrada = new BufferedReader(new FileReader(nomeArquivo));
+		}
+		catch (FileNotFoundException excecao) {
+			System.out.println("Arquivo não encontrado");
+		}
+	}
+	
+	public void fecharArquivo() {
+		
+		try {
+			entrada.close();
+		}
+		catch (IOException excecao) {
+			System.out.println("Erro no fechamento do arquivo de leitura: " + excecao);	
+		}
+	}
+	
+	public String ler() {
+		
+		String textoEntrada;
+		
+		try {
+			textoEntrada = entrada.readLine();
+		}
+		catch (EOFException excecao) { 
+			return null;
+		}
+		catch (IOException excecao) {
+			System.out.println("Erro de leitura: " + excecao);
+			return null;
+		}
+		return textoEntrada;
+	}
+}
