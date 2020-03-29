6b95e50e6cffd6be6ff6858a8be56e23657a13bc
Savio Pires <saviopires29@gmail.com>
2020-03-29 14:28:56 -0300

    arquitetura codigo

---
+++ src/Dictionary/Code.java
@@ -0,0 +1,56 @@
+package Dictionary;
+
+public class Code {
+	
+	public String DictionaryCode (String code) {
+		switch (code) {
+		case "ori":
+			return "001101";
+		case "jr":
+			return "001000";
+		case "jal":
+			return "000011";
+		case "add":
+			return "100000";
+		case "addi":
+			return "001000";
+		case "xor":
+			return "100110";
+		case "lw":
+			return "100011";
+		case "sw":
+			return "101011";
+		case "beq":
+			return "000100";
+		case "bne":
+			return "000101";
+		case "j":
+			return "000010";
+		case "sub":
+			return "100010";
+		case "mult":
+			return "011000";
+		case "div":
+			return "011010";
+		case "and":
+			return "100100";
+		case "andi":
+			return "001100";
+		case "or":
+			return "100101";			
+		case "addiu":
+			return "001001";
+		case "jalr":
+			return "001001";
+		case "slti":
+			return "001010";
+		case "movz":
+			return "001010";
+		case "sltiu":
+			return "001011";	
+			
+		}
+		return null;
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 
-
 import Archives.GravarArquivo;
 import Archives.LeituraArquivo;
-import Assembly_Binario.Binario;
+import Compiler.Binario;
+import Dictionary.Code;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		LeituraArquivo ler = new LeituraArquivo();
-		GravarArquivo gravar = new GravarArquivo();           
+		GravarArquivo gravar = new GravarArquivo();
 		Binario binario = new Binario();
 
 		ler.abrirArquivo("type-i.in", "/home/savio-pires/Documentos/faculdade/JSembler/");
-		gravar.abrirArquivo("type-i.out", "/home/savio-pires/Documentos/faculdade/JSembler/"); 
+		gravar.abrirArquivo("type-i.out", "/home/savio-pires/Documentos/faculdade/JSembler/");
 		String linhaCodigo = "";
 		String textoEntrada = "";
 		do {
@@ -21,24 +21,22 @@ public class Main {
 			if (linhaCodigo != null) {
 				char[] codigo = linhaCodigo.toCharArray();
 				linhaCodigo = "";
-				for (int i = 0; i < codigo.length; i++) {		
-					
-					
-					
-					if(codigo[i] == ')') {
-						System.out.println("textoo -->" + linhaCodigo);	
+				for (int i = 0; i < codigo.length; i++) {
+
+					if (codigo[i] == ')') {
+						System.out.println("textoo -->" + linhaCodigo);
 						textoEntrada += binarios(linhaCodigo);
 						linhaCodigo = "";
-					}else if (codigo[i] != ' ') {
+					} else if (codigo[i] != ' ') {
 						linhaCodigo += codigo[i];
-				         linhaCodigo = linhaCodigo.replace(",", "");           
-				         linhaCodigo = linhaCodigo.replace(" ", "");				         	
-				         if(isInteger(linhaCodigo)) {
-								int numCodigo = Integer.parseInt(linhaCodigo);						
-								textoEntrada += binario.NumBinario(numCodigo);
-								linhaCodigo = "";
-				         }
-					}else {
+						linhaCodigo = linhaCodigo.replace(",", "");
+						linhaCodigo = linhaCodigo.replace(" ", "");
+						if (isInteger(linhaCodigo)) {
+							int numCodigo = Integer.parseInt(linhaCodigo);
+							textoEntrada += binario.NumBinario(numCodigo);
+							linhaCodigo = "";
+						}
+					} else {
 						textoEntrada += binarios(linhaCodigo);
 						linhaCodigo = "";
 					}
@@ -50,25 +48,25 @@ public class Main {
 		gravar.fecharArquivo();
 		ler.fecharArquivo();
 	}
-	
-    private static boolean isInteger(String str) {
-        return str != null && str.matches("[0-9]*");
-    }
-    
-    
-    private static String binarios(String linha) {
-    	Binario binario = new Binario();
-    	
-    	linha = linha.replace("(", "");
-    	linha = linha.replace(")", "");
-        System.out.println(linha +" ----- "); 
-		System.out.println(linha +" ----- ");   
-		
-		if (binario.Assembly_Para_Binario(linha) == "inexistente") {
-	        return binario.Registradores_Binario(linha);
+
+	private static boolean isInteger(String str) {
+		return str != null && str.matches("[0-9]*");
+	}
+
+	private static String binarios(String linha) {
+		Code dictionary = new Code();
+
+		linha = linha.replace("(", "");
+		linha = linha.replace(")", "");
+		System.out.println(linha + " ----- ");
+		System.out.println(linha + " ----- ");
+
+		if (dictionary.DictionaryCode(linha) == "inexistente") {
+			// return binario.Registradores_Binario(linha);
 		} else {
-		  System.out.println("Codigo -> " + linha);
-		  return binario.Assembly_Para_Binario(linha);
+			System.out.println("Codigo -> " + linha);
+			return dictionary.DictionaryCode(linha);
 		}
-    }
+		return linha;
+	}
 }
