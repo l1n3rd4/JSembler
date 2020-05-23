ca1d935fa68cf25cf41ddd52dadac043d8810f2e
Leo <l1n3rd4@localhost.localdomain>
2020-05-23 16:50:04 -0300

    refactoring

--- src/Compiler/Binario.java
+++ src/Compiler/Binario.java
@@ -42,34 +42,7 @@ public class Binario {
 		return imediato;
 	}
 	
-	public String Instruction_R_Type() {
-		String retorno;
-		retorno = getOpcode() + Register.BinaryRegisters(registers[0]) + Register.BinaryRegisters(registers[1]) + 
-				Register.BinaryRegisters(registers[2]) + shamt + getFunct();
-		
-		System.out.println("String retorno: " + retorno);
-		return retorno;
-	}
 	
-	public String Instruction_I_Type(String comando) {
-		String retorno;
-		retorno = getOpcode() + Register.BinaryRegisters(registers[0])
-		+ Register.BinaryRegisters(registers[1]) + imediato;
-		
-		System.out.println("String retorno: " + retorno);
-		return retorno;
-	}
-	
-	
-	public String Instruction_J_Type(String comando) {
-		String retorno;
-		System.out.println("Tipo :" + getType());
-		retorno = getOpcode() + getAddress();
-		
-		System.out.println("Endereço: " + getAddress());
-		System.out.println("String de saída: " + retorno);
-		return retorno;
-	}
 
 	public String NumBinario(int decimal) {
 		String bin = "";
@@ -150,10 +123,6 @@ public class Binario {
 			this.address += endereco;
 		}
 		
-	}
-	
-	public String complete() {
-		return null;
 	}
 
 	public String getFunct() {
