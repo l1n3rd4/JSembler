76e5a35c59d87d3ecc9fb5384d9f890e3583f417
Leonardo Henrique Guimaraes <leonardo.guimaraes20@icloud.com>
2020-08-27 21:48:58 -0300

    Updating ReadArchive constructor

--- src/main/java/src/Archives/ReadArchieve.java
+++ src/main/java/src/Archives/ReadArchieve.java
@@ -9,28 +9,35 @@ public class ReadArchieve {
 	//Atributos para manipulacao de binario e tamanho
 	private File fileInput;
 	private int fileSize = 0;
+
 	//Verificar se existe palavra no arquivo em atributo passado como parametro utilizando throws
 	public ReadArchieve(String fileName) throws FileNotFoundException {
+		setFileInput(fileName);
+	}
 
+	//Passar para file o arquivo de entrada para leitura
+	public ReadArchieve(File file) throws FileNotFoundException {
+		if (file != null) {
+			fileInput = file;
+		} else {
+			throw new FileNotFoundException();
+		}
+	}
+
+	//Retorna o arquivo de entrada
+	public File getFileInput() {
+		return fileInput;
+	}
+
+	//Setar arquivo de entrada utilizando o atributo passado como parametro
+	public void setFileInput(String fileName) throws FileNotFoundException{
 		if (fileName != null) {
 			this.fileInput = new File(fileName);
 		} else {
 			throw new FileNotFoundException();
 		}
+	}
 
-	}
-	//Passar para file o arquivo de entrada para leitura  
-	public ReadArchieve(File file) {
-		fileInput = file;
-	}
-	//Retorna o arquivo de entrada
-	public File getFileInput() {
-		return fileInput;
-	}
-	//Setar arquivo de entrada utilizando o atributo passado como parametro
-	public void setFileInput(String fileName) {
-		this.fileInput = new File(fileName);
-	}
 	//Obter o tamanho do arquivo em array buscando por collection
 	public int getArrayLength() throws FileNotFoundException {
 		Scanner fileScanner = new Scanner(fileInput);
